66e9641acc5032d13ae9c07692b10e5e34d2b416
Kevin Karelse <219000859@mycput.ac.za>
2021-03-31 15:24:35 +0200

    Added Object testing

--- src/test/java/za/ac/cput/MultiplicationTest.java
+++ src/test/java/za/ac/cput/MultiplicationTest.java
@@ -1,7 +1,57 @@
 package za.ac.cput;
 
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MultiplicationTest {
+class MultiplicationTest{
+
+
+
+
+    private Multiplication number1;
+    private Multiplication number2;
+
+
+
+
+    @BeforeEach
+    void setUp() {
+        number1 = new Multiplication();
+        number2 = new Multiplication();
+
+    }
+
+    @Test
+    void testIdentity(){
+        assertSame(number1,number1);
+
+    }
+
+    @Test
+    void testEquality(){
+        assertEquals(number1, number1);
+
+    }
+
+    @Test
+    void failTest(){
+        assertEquals(number1,number2);
+
+    }
+
+    @Test
+    void timeoutTest(){
+
+
+    }
+
+
+
+
+
 
 }
