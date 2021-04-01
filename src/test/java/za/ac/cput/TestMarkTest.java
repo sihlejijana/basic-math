cfac925a761d626a93c8e0388e33e81eabcbddc5
User <217238173@mycput.ac.za>
2021-04-01 03:58:28 +0200

    Commit 2

---
+++ src/test/java/za/ac/cput/TestMarkTest.java
@@ -0,0 +1,59 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Timeout;
+import static java.time.Duration.ofMinutes;
+import static java.time.Duration.ofMillis;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled("Do not run Environment Below")
+class TestMarkTest {
+
+private TestMark Mark1;
+private TestMark Mark2;
+private TestMark Mark3;
+
+    @BeforeEach
+    void setUp()
+    {
+Mark1 = new TestMark();
+Mark2 = new TestMark();
+Mark1 = Mark3;
+    }
+
+    @Test
+    void testIdentity()
+    {
+assertSame(Mark1 , Mark2);
+    }
+
+    @Test
+    void testEquality()
+    {
+assertEquals(Mark1, Mark2);
+    }
+
+    @Test
+    void timeoutNotExceeded()
+    {
+        //asserTimeout passes
+        assertTimeout(ofMinutes(1), () ->){
+
+    });
+    }
+
+    @Test
+    void timeoutExceeds()
+    {
+        //asserTimeot fails
+        assertTimeout(ofMillis(5), () ->){
+
+            Thread.sleep(100);
+
+    });
+    }
+
+}
